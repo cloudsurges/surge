46926dba461f3ba619cef582d4590f3858dc964b
cloudsurge <96812044+cloudsurges@users.noreply.github.com>
2023-08-28 12:25:57 +0800

    Update ipv6.js

--- ipv6.js
+++ ipv6.js
@@ -21,12 +21,12 @@ const switchModule = (enable_module_name, disable_module_name) => {
 getModuleStatus().then((module_status) => {
   if (specific_wifi && (!module_status[0] || module_status[1])) {
     // 特定wifi下不使用ipv6
-    $notification.post('不启用IPv6', '', '')
-    switchModule(Ipv6_Cancel, Ipv6_Enable);
+    $notification.post('关闭IPv6', '', '')
+    switchModule(Ipv6_Cancel, Ipv6_Cancel);
   } else if (!specific_wifi && (module_status[0] || !module_status[1])) {
     // 一般情况下使用ipv6
     $notification.post('启用IPv6', '', '')
-    switchModule(Ipv6_Enable, Ipv6_Cancel);
+    switchModule(Ipv6_Enable, Ipv6_Enable);
   } else {
     // 重複觸發 => 結束
     // $notification.post('重複觸發','','')
