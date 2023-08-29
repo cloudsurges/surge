202bdaf3dd8d42a5b013d94fec2d3a3c2b439561
cloudsurge <96812044+cloudsurges@users.noreply.github.com>
2023-08-29 09:20:42 +0800

    Update ipv6.js

--- ipv6.js
+++ ipv6.js
@@ -19,26 +19,17 @@ const switchModule = (enable_module_name, disable_module_name) => {
 }
 
 getModuleStatus().then((module_status) => {
-//  if (specific_wifi && (!module_status[0] || module_status[1])) {
-//    // 特定wifi下不使用ipv6
-//    $notification.post('关闭IPv6', '', '')
-//    switchModule(Ipv6_Cancel, Ipv6_Enable);
-//  } else if (!specific_wifi && (module_status[0] || !module_status[1])) {
-//    // 一般情况下使用ipv6
-//    $notification.post('启用IPv6', '', '')
-//    switchModule(Ipv6_Enable, Ipv6_Cancel);
-//  } else {
-//    // 重複觸發 => 結束
-//    // $notification.post('重複觸發','','')
- //   $done();
- // }
-  if (specific_wifi)  {
+  if (specific_wifi && (!module_status[0] || module_status[1])) {
+    // 特定wifi下不使用ipv6
     $notification.post('关闭IPv6', '', '')
     switchModule(Ipv6_Cancel, Ipv6_Enable);
-    $done();
-    } else {
+  } else if (!specific_wifi && (module_status[0] || !module_status[1])) {
+    // 一般情况下使用ipv6
     $notification.post('启用IPv6', '', '')
     switchModule(Ipv6_Enable, Ipv6_Cancel);
+  } else {
+    // 重複觸發 => 結束
+    // $notification.post('重複觸發','','')
     $done();
   }
 })
