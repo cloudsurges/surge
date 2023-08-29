fd0cdb45c1123460c424466a0a52c77e40ef30bc
cloudsurge <96812044+cloudsurges@users.noreply.github.com>
2023-08-29 11:44:01 +0800

    Update IPv6.js

--- IPv6.js
+++ IPv6.js
@@ -1,12 +1,12 @@
-const Ipv6_Cancel = 'Ipv6-Cancel';
-const Ipv6_Enable = 'Ipv6-Enable';
+const IPv6_Cancel = 'IPv6-Cancel';
+const IPv6_Enable = 'IPv6-Enable';
 const specific_wifi = $network.wifi.ssid === 'jawave-5G' || $network.wifi.ssid === 'jawave02-5G';
  
 function getModuleStatus() {
   return new Promise((resolve) => {
     $httpAPI('GET', 'v1/modules', null, (data) => {
       let enabled = data.enabled;
-      resolve([enabled.includes(Ipv6_Cancel), enabled.includes(Ipv6_Enable)]);
+      resolve([enabled.includes(IPv6_Cancel), enabled.includes(IPv6_Enable)]);
     });
   });
 }
@@ -20,16 +20,16 @@ const switchModule = (enable_module_name, disable_module_name) => {
  
 getModuleStatus().then((module_status) => {
   if (specific_wifi && (!module_status[0] || module_status[1])) {
-    // 在家，用本地 DNS
+    // 在特定网络下关闭IPv6
     $notification.post('关闭IPv6', '', '')
-    switchModule(Ipv6_Cancel, Ipv6_Enable);
+    switchModule(IPv6_Cancel, IPv6_Enable);
   } else if (!specific_wifi && (module_status[0] || !module_status[1])) {
-    // 不在家，使用 DoH
+    // 默认的情况下开启IPv6
     $notification.post('开启IPv6', '', '')
-    switchModule(Ipv6_Enable, Ipv6_Cancel);
+    switchModule(IPv6_Enable, IPv6_Cancel);
   } else {
-    // 重複觸發 => 結束
-    // $notification.post('重複觸發','','')
+    // 重复触发 => 结束
+    $notification.post('特定WIFI环境下关闭IPv6的模块条件重复触发！','','')
     $done();
   }
 })
