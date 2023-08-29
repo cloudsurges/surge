1f727194a9d47eb544ce2d93c5264408ca3ffd45
cloudsurge <96812044+cloudsurges@users.noreply.github.com>
2023-08-29 13:53:52 +0800

    Update IPv6-Adaptive.js

--- IPv6-Adaptive.js
+++ IPv6-Adaptive.js
@@ -4,6 +4,13 @@ const specific_wifi = $network.wifi.ssid === 'jawave-5G' || $network.wifi.ssid =
 
 let ip6addr = (typeof $network.v6 != 'undefined') && (typeof $network.v6.primaryAddress != 'undefined') ? $network.v6.primaryAddress : '';
 
+if(ip6addr){
+    $notification.post('hahahahahah', '', '')
+    $done();
+}else{
+  $done();
+}
+
 function getModuleStatus() {
   return new Promise((resolve) => {
     $httpAPI('GET', 'v1/modules', null, (data) => {
@@ -24,7 +31,6 @@ getModuleStatus().then((module_status) => {
   if (specific_wifi && (!module_status[0] || module_status[1])) {
     // 在特定网络下关闭IPv6
     $notification.post('关闭IPv6', '', '')
-    $notification.post($ip6addr, '', '')
     switchModule(IPv6_Cancel, IPv6_Enable);
   } else if (!specific_wifi && (module_status[0] || !module_status[1])) {
     // 默认的情况下开启IPv6
