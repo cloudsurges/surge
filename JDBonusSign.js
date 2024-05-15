49a9fde476aee19231feb7eaf22bcd507c859c9d
cloudsurge <96812044+cloudsurges@users.noreply.github.com>
2024-05-16 02:29:15 +0800

    Update JDBonusSign.js

--- JDBonusSign.js
+++ JDBonusSign.js
@@ -1,7 +1,11 @@
-const request = {
-  url: "https://api.m.jd.com/client.action?functionId=signBeanAct&appid=ld"
+const RequestURL = {
+  url: "https://api.m.jd.com/client.action?functionId=signBeanAct&appid=ld",
+  # headers: {
+  #  Cookie: KEY
+  # },
+  # body: 'functionId=signBeanAct&appid=ld'
 }
-$httpClient.post(request, (error, response, data) => {
-  console.log("发送签到请求")
+$httpClient.post(RequestURL, (error, response, data) => {
+  console.log(`访问状态：${response.status}`)
   $done()
 })
